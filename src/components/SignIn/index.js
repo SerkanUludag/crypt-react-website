449cc8b47c48818841f87ed8e9af1d7e17d88555
SerkanUludag <hsuluda1@gmail.com>
2021-02-28 17:15:08 +0300

    misspell fixed

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -17,7 +17,7 @@ const SignIn = () => {
     <>
       <Container>
         <FormWrap>
-          <Icon to="/">dolla</Icon>
+          <Icon to="/">crypt</Icon>
           <FormContent>
             <Form action="#">
               <FormH1>Sign in to your account</FormH1>
